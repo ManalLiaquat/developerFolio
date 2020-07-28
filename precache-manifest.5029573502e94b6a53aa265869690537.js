2d3f8b79a065a71ab22c04941f3b3801127546cb
Manal Liaquat <m.manalliaquat@gmail.com>
2020-07-28 16:43:10 +0500

    Updates

---
+++ precache-manifest.5029573502e94b6a53aa265869690537.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e74287abcdc19576baa8d5967fc2eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21eb39447e295bb53006",
+    "url": "/static/css/3.b499fcd2.chunk.css"
+  },
+  {
+    "revision": "095749fa52c4211f1b09",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "e8782320487359bff7c6",
+    "url": "/static/css/main.e1060450.chunk.css"
+  },
+  {
+    "revision": "9dcc13b18d0ee566dd02",
+    "url": "/static/js/2.8e5ac334.chunk.js"
+  },
+  {
+    "revision": "21eb39447e295bb53006",
+    "url": "/static/js/3.1cfb55b9.chunk.js"
+  },
+  {
+    "revision": "095749fa52c4211f1b09",
+    "url": "/static/js/4.ce1f5896.chunk.js"
+  },
+  {
+    "revision": "e8782320487359bff7c6",
+    "url": "/static/js/main.412a40ec.chunk.js"
+  },
+  {
+    "revision": "33d690247529006cb6fe",
+    "url": "/static/js/runtime-main.03c62170.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "6ac0c6518626a6d0234b65b0991c5106",
+    "url": "/static/media/digitalMarknAdsPlan.6ac0c651.webp"
+  },
+  {
+    "revision": "f3b5e4685561a67dff6f4915dd83fea4",
+    "url": "/static/media/freecodecamp.f3b5e468.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "c4bbd74c240882bf17b38c0598b7ed5c",
+    "url": "/static/media/smit.c4bbd74c.webp"
+  },
+  {
+    "revision": "325dfdf5a1a3f00b50a270a8dd52e18e",
+    "url": "/static/media/systemsAvenue.325dfdf5.webp"
+  },
+  {
+    "revision": "2e59467e0a924c7a8ec31e51a8a579fb",
+    "url": "/static/media/udacity.2e59467e.png"
+  }
+]);
